e0c733850be385d7a0b83536995f7c17caa81625
Scott Kirkwood <scott@forusers.com>
2011-11-12 10:43:18 -0500

    Added move active torrent to backup drive.

--- watched.py
+++ watched.py
@@ -6,10 +6,11 @@ from twisted.internet import reactor
 # Set up the logger to print out errors
 from deluge.log import setupLogger
 
-import re
-import time
-import os.path
 import optparse
+import os.path
+import re
+import shutil
+import time
 
 class Deluge:
     def __init__(self, options, fnames):
@@ -18,12 +19,14 @@ class Deluge:
         self._deluge.addCallback(self.on_connect_success)
         self._deluge.addErrback(self.on_error)
         self.watched_path = self.ensure_exists(options['watched-local'])
+        self.towatch_remote_path = self.ensure_exists(options['towatch-remote'])
         self.fnames = fnames
         self.re_fnames = []
         for fname in fnames:
             self.re_fnames.append(re.compile(fname, re.IGNORECASE))
-        self.to_move = []
-        self.to_remove = []
+        self.to_move_storage = []  # keep torrent, move data
+        self.to_move_backup = []  # remove torrent, move data to backup
+        self.to_remove = []  # remove torrent and data
         self.options = options
         self.finish_location = None
 
@@ -34,7 +37,7 @@ class Deluge:
 
     def ensure_exists(self, directory):
         if not os.path.isdir(directory):
-            os.mkdirs(directory)
+            os.makedirs(directory)
         return directory
 
     def done(self):
@@ -42,6 +45,18 @@ class Deluge:
         client.disconnect()
         # Stop the twisted main loop and exit
         reactor.stop()
+
+    def on_move_storage(self, unused_info):
+        if self.to_move_storage:
+            self.move_storage()
+        else:
+            time.sleep(1)
+            self.done()
+
+    def move_storage(self):
+        hash_val, fname = self.to_move_storage.pop()
+        print 'Moving %s to %s' % (fname, self.watched_path)
+        client.core.move_storage([hash_val], self.watched_path).addCallback(self.on_move_storage)
 
     def on_get_torrents_move_storage(self, torrents):
         found = []
@@ -57,33 +72,13 @@ class Deluge:
                             if self.watched_path in save_path:
                                 print '%s already in watched path' % fname
                             else:
-                                found.append((hashval, fname))
+                                found.append((hashval, torrent_info['files']))
         if len(found) == len(self.fnames):
-            self.to_move = found[:]
+            self.to_move_storage = found[:]
             self.move_storage()
         elif not found:
             if not found_one:
                 print '%s not found' % ', '.join(self.fnames)
-            self.done()
-        else:
-            print 'Found %d matches for %s' % (len(found), ', '.join(self.fnames))
-            self.done()
-
-    def on_get_torrents_remove_torrrent(self, torrents):
-        found = []
-        found_one = False
-        for hashval, torrent_info in torrents.iteritems():
-            if torrent_info['progress'] == 100.0:
-                for file_info in torrent_info['files']:
-                    fname = file_info['path']
-                    for re_fname in self.re_fnames:
-                        if re_fname.search(fname):
-                            found.append((hashval, fname))
-        if len(found) == len(self.fnames):
-            self.to_remove = found[:]
-            self.remove_torrent()
-        elif not found:
-            print '%s not found' % ', '.join(self.fnames)
             self.done()
         else:
             print 'Found %d matches for %s' % (len(found), ', '.join(self.fnames))
@@ -124,18 +119,7 @@ class Deluge:
 
         self.done()
 
-    def on_move_storage(self, unused_info):
-        if self.to_move:
-            self.move_storage()
-        else:
-            time.sleep(1)
-            self.done()
-
-    def move_storage(self):
-        hash_val, fname = self.to_move.pop()
-        print 'Moving %s to %s' % (fname, self.watched_path)
-        client.core.move_storage([hash_val], self.watched_path).addCallback(self.on_move_storage)
-
+    # Remove torrent
     def on_remove_torrent(self, unused_info):
         if self.to_remove:
             self.remove_torrent()
@@ -148,6 +132,68 @@ class Deluge:
         print 'Removing torrent and data %s' % (fname)
         client.core.remove_torrent(hash_val, remove_data=True).addCallback(self.on_remove_torrent)
 
+    def on_get_torrents_remove_torrent(self, torrents):
+        found = []
+        found_one = False
+        for hashval, torrent_info in torrents.iteritems():
+            if torrent_info['progress'] == 100.0:
+                for file_info in torrent_info['files']:
+                    fname = file_info['path']
+                    for re_fname in self.re_fnames:
+                        if re_fname.search(fname):
+                            found.append((hashval, fname))
+        if len(found) == len(self.fnames):
+            self.to_remove = found[:]
+            self.remove_torrent()
+        elif not found:
+            print '%s not found' % ', '.join(self.fnames)
+            self.done()
+        else:
+            print 'Found %d matches for %s' % (len(found), ', '.join(self.fnames))
+            self.done()
+
+    # Move torrent data to backup
+    def on_move_torrent_data(self, unused, torrent_info):
+        save_path = torrent_info['save_path']
+        for file_info in torrent_info['files']:
+            fname = file_info['path']
+            from_fname = os.path.join(save_path, fname)
+            to_fname = os.path.join(self.towatch_remote_path, fname)
+            print 'Moving %s to %s' % (fname, self.towatch_remote_path)
+            shutil.move(from_fname, to_fname)
+
+        if self.to_move_backup:
+            self.move_torrent_data()
+        else:
+            time.sleep(1)
+            self.done()
+
+    def move_torrent_data(self):
+        hash_val, torrent_info = self.to_move_backup.pop()
+        print ('Removing torrent %s' % torrent_info['files'][0]['path'])
+        client.core.remove_torrent(hash_val, remove_data=False).addCallback(
+                self.on_move_torrent_data, torrent_info)
+
+    def on_get_torrents_make_space(self, torrents):
+        found = []
+        found_one = False
+        for hashval, torrent_info in torrents.iteritems():
+            if torrent_info['progress'] == 100.0:
+                for file_info in torrent_info['files']:
+                    fname = file_info['path']
+                    for re_fname in self.re_fnames:
+                        if re_fname.search(fname):
+                            found.append((hashval, torrent_info))
+        if found and len(found) == len(self.fnames):
+            self.to_move_backup = found[:]
+            self.move_torrent_data()
+        elif not found:
+            print '%s not found' % ', '.join(self.fnames)
+            self.done()
+        else:
+            print 'Found %d matches for %s' % (len(found), ', '.join(self.fnames))
+            self.done()
+
     # We create a callback function to be called upon a successful connection
     def on_connect_success(self, result):
         # Request the config value for the key 'finish_location'
@@ -158,7 +204,9 @@ class Deluge:
         if self.options['orphans']:
             callback = self.on_get_torrents_orphans
         elif self.fnames and self.options['remove']:
-            callback = self.on_get_torrents_remove_torrrent
+            callback = self.on_get_torrents_remove_torrent
+        elif self.fnames and self.options['make-space']:
+            callback = self.on_get_torrents_make_space
         elif self.fnames:
             callback = self.on_get_torrents_move_storage
         else:
@@ -176,12 +224,17 @@ if __name__ == '__main__':
         help='Files that don\'t belong')
     parser.add_option(
         '-r', '--rm', dest='remove', action='store_true',
-        help='Remove the torrent, move to /media../watched or /media/..towatch')
+        help='Remove the torrent and data')
+    parser.add_option(
+        '-m', '--move', dest='make_space', action='store_true',
+        help='Make space, move to hard disk, remove .torrent')
     options, args = parser.parse_args()
     options = {
             'watched-local': os.path.expanduser('~/finished/watched'),
+            'towatch-remote': os.path.expanduser('/media/backup/videos/towatch'),
             'orphans': options.orphans,
-            'remove': options.remove
+            'remove': options.remove,
+            'make-space': options.make_space,
     }
     d = Deluge(
             options,
