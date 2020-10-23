43b5eefd37be38ddd6190c931fbe51c8854f9b3d
Scott Kirkwood <scott@forusers.com>
2020-10-23 17:44:16 -0400

    Remove old mercurial files.

--- hd-health.sh
+++ hd-health.sh
@@ -2,6 +2,6 @@
 echo "/dev/sda1"
 sudo hddtemp /dev/sda1
 sudo smartctl --health /dev/sda1 | tail -1
-echo "/dev/sdb2"
-sudo hddtemp /dev/sdb2
-sudo smartctl --health /dev/sdb2 | tail -1
+echo "/dev/sdb1"
+sudo hddtemp /dev/sdb1
+sudo smartctl --health /dev/sdb1 | tail -1
